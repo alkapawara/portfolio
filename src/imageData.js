357eea839ba92b94b3f024d33f179b1fcb85125e
alkapawara <alkapawara749@gmail.com>
2024-11-30 11:45:25 +0530

    adding right path of image

--- src/imageData.js
+++ src/imageData.js
@@ -4,7 +4,7 @@ import brandcare from "../src/assets/brandcare.png";
 import prasadam from "../src/assets/prasadam.png";
 import todolist from "../src/assets/todo-list.png";
 import keshawshrusthi from "../src/assets/keshawshrusthi.png";
-import profileImg from "../src/assets/alka-photo.png";
+import profileImg from "../src/assets/alka-photo.jpg";
 const imageData = {
   docintosh,
   ecommerce,
