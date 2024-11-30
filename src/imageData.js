ba2b498556e315df5315af580bc64850fd0f71c0
alkapawara <alkapawara749@gmail.com>
2024-11-30 11:24:00 +0530

    image file data change

--- src/imageData.js
+++ src/imageData.js
@@ -1,11 +1,17 @@
+import docintosh from "../src/assets/web.png";
+import ecommerce from "../src/assets/ecommerce.png";
+import brandcare from "../src/assets/brandcare.png";
+import prasadam from "../src/assets/prasadam.png";
+import todolist from "../src/assets/todo-list.png";
+import keshawshrusthi from "../src/assets/keshawshrusthi.png";
+
 const imageData = {
-    docintosh: "../src/assets/web.png",
-    ecommerce: "../src/assets/ecommerce.png",
-    brandcare: "../src/assets/brandcare.png",
-    prasadam: "../src/assets/prasadam.png",
-    todolist: "../src/assets/todo-list.png",
-    keshawshrusthi: "../src/assets/keshawshrusthi.png",
-  };
-  
-  export default imageData;
-  
+  docintosh,
+  ecommerce,
+  brandcare,
+  prasadam,
+  todolist,
+  keshawshrusthi,
+};
+
+export default imageData;
